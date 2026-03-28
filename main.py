89149a858ca1eb7077e6d8508717eb255a648acb
CaroNorambuena <carolinanorambuena.l@gmail.com>
2026-03-28 13:29:16 -0300

    Se agrega IU

--- main.py
+++ main.py
@@ -1,63 +1,87 @@
+
 import cv2
 from pyzbar.pyzbar import decode
 
-# Cargar imagen
-imagen = cv2.imread("codigo.png") # Foto tomada con el celular
 
-# Verificar que la imagen se cargó correctamente
-if imagen is None:
-    print("Error: No se pudo cargar la imagen.")
-    exit() # Detener la ejecución
+def codigo():
+        # INPUT USUARIO
+        nombre_archivo = entrada.get() #con get se obtiene lo que se escribe en "entrada" y se guarda como nombre_archivo
 
-# Convertir a escala de grises (mejora detección)
-gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+        # Cargar imagen
+        imagen = cv2.imread(nombre_archivo) # Foto tomada con el celular
 
-# Detectar códigos de barras
-codigos = decode(gris) # Devuelve una lista de objetos. Cada uno tiene atributos
-print(codigos)
+        # Verificar que la imagen se cargó correctamente
+        if imagen is None:
+            print("Error: No se pudo cargar la imagen.")
+            # exit() # Detener la ejecución
 
-if not codigos:
-    print("No se detectó ningún código de barras.")
-else:
-    # Iterar la lista codigos. Cada elemento de la lista se llama codigo
-    for codigo in codigos: 
-        # Extraer datos
-        datos = codigo.data.decode("utf-8") # Extrae el atributo data y luego los muestra en UTF-8
-        tipo = codigo.type # Extrae el atributo type
+        # Convertir a escala de grises (mejora detección)
+        gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 
-        print(f"Código detectado: {datos}")
-        print(f"Tipo: {tipo}")
+        # Detectar códigos de barras
+        codigos = decode(gris) # Devuelve una lista de objetos. Cada uno tiene atributos
+        print(codigos)
 
-        # # Dibujar rectángulo alrededor del código
-        # x, y, w, h = codigo.rect
-        # cv2.rectangle(imagen, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if not codigos:
+            print("No se detectó ningún código de barras.")
+        else:
+            # Iterar la lista codigos. Cada elemento de la lista se llama codigo
+            for codigo in codigos: 
+                # Extraer datos
+                datos = codigo.data.decode("utf-8") # Extrae el atributo data y luego los muestra en UTF-8
+                tipo = codigo.type # Extrae el atributo type
 
-        # # Mostrar texto encima
-        # cv2.putText(imagen, datos, (x, y - 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX,
-        #             0.5, (0, 255, 0), 2)
+                print(f"Código detectado: {datos}")
+                print(f"Tipo: {tipo}")
+
+                # # Dibujar rectángulo alrededor del código
+                # x, y, w, h = codigo.rect
+                # cv2.rectangle(imagen, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # # Mostrar texto encima
+                # cv2.putText(imagen, datos, (x, y - 10),
+                #             cv2.FONT_HERSHEY_SIMPLEX,
+                #             0.5, (0, 255, 0), 2)
 
 
-import mysql.connector as db
+        import mysql.connector as db
 
-# Conexión
-mydb = db.connect(
-    host="localhost",
-    user="root",
-    port = 3306,
-    passwd="mysql123",
-    database="Inventario"
-)
+        # Conexión
+        mydb = db.connect(
+            host="localhost",
+            user="root",
+            port = 3306,
+            passwd="mysql123",
+            database="Inventario"
+        )
 
-cursor = mydb.cursor()
+        cursor = mydb.cursor()
 
-# Consulta
-sql = "SELECT nombre, precio FROM productos WHERE código = %s"
-# Nota: la coma es obligatoria en SQL para convertir la variable en tupla
-valor = (datos,) 
+        # Consulta
+        sql = "SELECT nombre, precio FROM productos WHERE código = %s"
+        # Nota: la coma es obligatoria en SQL para convertir la variable en tupla
+        valor = (datos,) 
 
-cursor.execute(sql, valor)
+        cursor.execute(sql, valor)
 
-resultado = cursor.fetchone()
+        resultado = cursor.fetchone()
 
-print(resultado)
+        #OUTPUT USUARIO
+        output.config(text=resultado) #el texto de esta etiqueta cambia a "resultado"
+
+    
+#########INTERFAZ USUARIO###########
+
+import tkinter as tk
+
+ventana = tk.Tk()
+
+entrada = tk.Entry(ventana)
+entrada.pack()
+
+tk.Button(ventana, text="Aceptar", command=codigo).pack()
+
+output = tk.Label(ventana, text="Producto: ")
+output.pack()
+
+ventana.mainloop()
